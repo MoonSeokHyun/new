3c203fc1cfec73064ccf3ed180b22f9a73e40ff4
문석현 <munseoghyeon@Mac.wlan>
2024-11-07 22:59:09 +0900

    s

--- app/Views/posts/create.php
+++ app/Views/posts/create.php
@@ -290,6 +290,13 @@
         menu.classList.toggle('active');
     }
 </script>
-
+<script type="text/javascript" src="//wcs.naver.net/wcslog.js"></script>
+<script type="text/javascript">
+if(!wcs_add) var wcs_add = {};
+wcs_add["wa"] = "8bcce9183d61c0";
+if(window.wcs) {
+wcs_do();
+}
+</script>
 </body>
 </html>
